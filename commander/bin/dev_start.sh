b8292d9f3c9e2dfb3bdd01dc3e09c0294d2681f6
John C. Linford <jlinford@paratools.com>
2015-02-03 09:20:34 -0500

    Passport.js user registraion.
    Former-commit-id: 05d9ec5c0ff5e5768a1209af9a12528159c5a0ac [formerly 7cb7580d7642a27314bef8e235b9c9f70a731a34] [formerly f6ab6f0ee95e54bc574e661f2cef843c57771bf7]
    Former-commit-id: 4b2a53db308f8021c20bc14bac55f7dbe70972b0
    Former-commit-id: 5b7f570edc91b7e31ce58b4ae8acc7bc7709b1ed

---
+++ commander/bin/dev_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd "$HERE/.."
+export PATH=$PWD/node_modules/.bin:$PATH
+
+export NODE_ENV="development"
+rm -rf .tmp
+sails lift $@
+
