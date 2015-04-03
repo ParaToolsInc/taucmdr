da04bdf9624446574a0291196c962e2ba2265140
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-04-02 22:09:24 -0600

    turning on BFD
    Former-commit-id: dbc07f051c1e2d64cbdea07595d0b295819364ee [formerly 6c907520245911d21e1076cf5115f3159319b1d6] [formerly 1c0e7b341c0d31346b4bbd730e4739e54b187f6a]
    Former-commit-id: 9451f65c7fb55023634ec6529bbd8cd0960189fb
    Former-commit-id: 9e92737e769b78cd27684fe0b57e3e03b86a41b8

--- packages/tau/model/experiment.py
+++ packages/tau/model/experiment.py
@@ -169,12 +169,15 @@ class Experiment(controller.Controller):
       pdt = cf.pdt.Pdt(prefix, cxx, target['pdt_source'], target['host_arch'])
       pdt.install()
       self.pdt = pdt
+      bfd = cf.bfd.Bfd(prefix, cxx, target['bfd_source'], target['host_arch'])
+      bfd.install()
+      self.bfd = bfd
 
     # Configure/build/install TAU if needed
     tau = cf.tau.Tau(prefix, cc, cxx, fc, target['tau_source'], target['host_arch'],
                      verbose=verbose,
                      pdt=pdt,
-                     bfd=None, # TODO
+                     bfd=bfd, 
                      libunwind=None, # TODO
                      profile=measurement['profile'],
                      trace=measurement['trace'],
