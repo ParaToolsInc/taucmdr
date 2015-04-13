9ba6798ea36206617f472157d012c90674d27ab3
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-04-13 09:11:26 -0600

    copyright adjusted
    Former-commit-id: 36d4a8774c3b2ca5f74efea8a578df52b511a0fd
    Former-commit-id: 9de4c8e1902444a89ec66c71c589cb9315f76ca9
    Former-commit-id: d62f259861181c18f08493fd8f36934ac4b8b11a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'TauCmdr'
-copyright = u'2015, John Linford  and Srinath Vadlamani @ ParaTools, Inc.'
+copyright = u'2015, ParaTools, Inc.'
 author = u'John Linford and Srinath Vadlamani @ ParaTools, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
