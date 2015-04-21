ffbfa6a76731a78f36bddba037e3babf9a2728df
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-04-21 17:47:35 -0600

    starting the compatibility class
    this may need to serve as compatibility class that model/experiment can use.
    
    
    Former-commit-id: 96ef7a071c03ebac0f3c20d9d28c3da512602d67
    Former-commit-id: 09223a44d8294098793d05fe0618f04139aabd03
    Former-commit-id: 37287b55b31ca617ba72a914c73f82eda80663ac

--- packages/tau/model/experiment.py
+++ packages/tau/model/experiment.py
@@ -12,26 +12,26 @@
 #Copyright (c) 2015, ParaTools, Inc.
 #All rights reserved.
 #
-#Redistribution and use in source and binary forms, with or without 
+#Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are met:
-# (1) Redistributions of source code must retain the above copyright notice, 
+# (1) Redistributions of source code must retain the above copyright notice,
 #     this list of conditions and the following disclaimer.
-# (2) Redistributions in binary form must reproduce the above copyright notice, 
-#     this list of conditions and the following disclaimer in the documentation 
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
 #     and/or other materials provided with the distribution.
-# (3) Neither the name of ParaTools, Inc. nor the names of its contributors may 
-#     be used to endorse or promote products derived from this software without 
+# (3) Neither the name of ParaTools, Inc. nor the names of its contributors may
+#     be used to endorse or promote products derived from this software without
 #     specific prior written permission.
 #
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
-#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #"""
 
@@ -65,7 +65,7 @@ class Experiment(controller.Controller):
   """
   Experiment data model controller
   """
-  
+
   attributes = {
     'project': {
       'model': 'Project',
@@ -88,7 +88,7 @@ class Experiment(controller.Controller):
       'via': 'experiment'
     },
   }
-  
+
   def name(self):
     populated = self.populate()
     return '%s (%s, %s, %s)' % (populated['project']['name'],
@@ -102,8 +102,8 @@ class Experiment(controller.Controller):
     """
     populated = self.populate()
     return os.path.join(populated['project'].prefix(),
-                        populated['target']['name'], 
-                        populated['application']['name'], 
+                        populated['target']['name'],
+                        populated['application']['name'],
                         populated['measurement']['name'])
 
   def onCreate(self):
@@ -114,7 +114,7 @@ class Experiment(controller.Controller):
     try:
       util.mkdirp(prefix)
     except:
-      raise error.ConfigurationError('Cannot create directory %r' % prefix, 
+      raise error.ConfigurationError('Cannot create directory %r' % prefix,
                                      'Check that you have `write` access')
 
   def onDelete(self):
@@ -134,7 +134,7 @@ class Experiment(controller.Controller):
     if not self.eid:
       raise error.InternalError('Tried to select an experiment without an eid')
     settings.set('experiment_id', self.eid)
-  
+
   def isSelected(self):
     if self.eid:
       return settings.get('experiment_id') == self.eid
@@ -227,20 +227,21 @@ class Experiment(controller.Controller):
     tau = cf.tau.Tau(prefix, cc, cxx, fc, target['tau_source'], target['host_arch'],
                      verbose=verbose,
                      pdt=pdt,
-                     bfd=bfd, 
-                     papi=papi, 
-                     libunwind=libunwind, 
+                     bfd=bfd,
+                     papi=papi,
+                     libunwind=libunwind,
                      profile=measurement['profile'],
                      trace=measurement['trace'],
                      sample=measurement['sample'],
                      source_inst=measurement['source_inst'],
                      compiler_inst=measurement['compiler_inst'],
-                     # TODO: Library wrapping inst
-                     openmp_support=application['openmp'], 
+                     # TODO: Library wrapping inst [dynamic]
+                     # TODO: binary rewrite  inst
+                     openmp_support=application['openmp'],
                      openmp_measurements=measurement['openmp'],
-                     pthreads_support=application['pthreads'], 
+                     pthreads_support=application['pthreads'],
                      pthreads_measurements=None, # TODO
-                     mpi_support=application['mpi'], 
+                     mpi_support=application['mpi'],
                      mpi_measurements=measurement['mpi'],
                      cuda_support=application['cuda'],
                      cuda_measurements=None, # Todo
@@ -269,7 +270,7 @@ class Experiment(controller.Controller):
     # Confirm target supports compiler
     if given_compiler.eid != target_compiler:
       raise error.ConfigurationError("Target '%s' is configured with %s compiler '%s', not '%s'",
-                                     (self['name'], given_compiler['language'], 
+                                     (self['name'], given_compiler['language'],
                                       given_compiler.absolutePath(),
                                       target_compiler.absolutePath()),
                                      "Use a different target or use compiler '%s'" %
@@ -303,12 +304,12 @@ class Experiment(controller.Controller):
     """
     self.configure()
     measurement = self.populate('measurement')
-    
+
     command = util.which(application_cmd)
     if not command:
       raise error.ConfigurationError("Cannot find executable: %s" % application_cmd)
     path = os.path.dirname(command)
-    
+
     # Check for existing profile files
     if measurement['profile']:
       profiles = glob.glob(os.path.join(path, 'profile.*.*.*'))
@@ -323,7 +324,7 @@ class Experiment(controller.Controller):
     # Build environment from component packages
     opts, env = environment.base()
     self.tau.applyRuntimeConfig(opts, env)
-    
+
     # TODO : Select tau_exec as needed
     use_tau_exec = False
     if use_tau_exec:
@@ -332,9 +333,9 @@ class Experiment(controller.Controller):
     else:
       cmd = [application_cmd]
     cmd += application_args
-    
+
     return Trial.perform(self, cmd, path, env)
-  
+
   def show(self, trial_numbers=None):
     """
     Show most recent trial or all trials with given numbers
@@ -359,11 +360,11 @@ class Experiment(controller.Controller):
             latest_date = trial['begin_time']
             latest_trial = trial
         trials = [trial]
-      
+
     if not trials:
       raise error.ConfigurationError("No trials in experiment %s" % self.name(),
                                      "See `tau trial create --help`")
-      
+
     opts, env = environment.base()
     self.tau.applyRuntimeConfig(opts, env)
 
