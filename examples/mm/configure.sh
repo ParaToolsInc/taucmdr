e94f0c1712572bef404b1fc44b759d6f50836ce8
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-05-29 10:02:26 -0600

    tuning for srinathv system for easier devel
    Former-commit-id: b3f0360ac66fa487b854d2730831c88a66d51af7
    Former-commit-id: 0583047dfdc0345827ef54e54456ea8b1756f712
    Former-commit-id: f19dfaee5b5a9714b9f9fa77ce5338e50a5532cb

--- examples/mm/configure.sh
+++ examples/mm/configure.sh
@@ -11,6 +11,20 @@ set +x
 # Example targets
 target_name="ex-`echo $HOSTNAME | cut -d. -f1`"
 tau target create "$target_name"
+user_name="`users`"
+
+sv_sys=false
+if [[ $target_name == *"Para"* ]] && [[ $user_name == *"srinath"* ]]
+then
+  sv_sys=true;
+  echo "This is a srinathv Paratools system"
+fi
+
+#if on srinath systems:
+if $sv_sys; then
+  echo "** making gcc5 a target with now bfd and libunwind"
+  tau target create gcc5 --compilers=GNU --with-bfd=False --with-libunwind=False
+fi
 
 # Example applications
 tau application create "ex-mm-serial"
@@ -21,11 +35,18 @@ tau application create "ex-mm-openmp-mpi" --openmp --mpi
 tau measurement create "ex-profile"
 tau measurement create "ex-trace" --profile=F --trace=T
 tau measurement create "ex-sample" --profile=F --sample=T
+tau measurement create "p-keep" --keep-inst-files=T
 
-# Set up example project 
+# Set up example project
 tau project create "ex-mm" \
   $target_name \
   ex-mm-serial ex-mm-openmp ex-mm-openmp-mpi \
   ex-profile ex-trace ex-sample
+
+if $sv_sys; then
+  echo "** making gcc target part of project"
+  tau project edit ex-mm --add-targets gcc5
+  tau project edit ex-mm --add-measurement p-keep
+fi
 
 tau dashboard
