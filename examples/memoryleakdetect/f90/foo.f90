3977ee80c9748e32c847477c787861b9d72ecdeb
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-06-01 16:46:53 -0600

    slow migration of tau examples for tau commander.  Will need to added more scripts for configuring
    Former-commit-id: ab4faee2312b7962e689c8439d6db88aa8f005db
    Former-commit-id: bb50d928de1cfa96a5f201d6bcbbef68b9c69fb1
    Former-commit-id: c55a31a63b594b3d25d383d753f429e8e8545b31

---
+++ examples/memoryleakdetect/f90/foo.f90
@@ -0,0 +1,27 @@
+module scratch
+   integer, parameter :: wp = selected_real_kind(15)
+   real(wp), allocatable :: A(:,:), B(:,:), C(:,:)   ! scratch arrays
+contains
+   subroutine allocate_scratch(n)
+      integer, intent(in) :: n
+      allocate( A(n,n), B(n,n), C(n,n) )
+      return
+   end subroutine allocate_scratch
+   subroutine use_scratch
+      ! use the module arrays A, B, and C.
+      call random_number( A ); call random_number( B )
+      C = matmul( A, B )
+      return
+   end subroutine use_scratch
+   subroutine deallocate_scratch
+      deallocate( A, B )  ! note that C is not included in this list.
+      return
+   end subroutine deallocate_scratch
+end module scratch
+
+program matrix
+   use scratch
+   call allocate_scratch( 1000 )
+   call use_scratch
+   call deallocate_scratch
+end program matrix
