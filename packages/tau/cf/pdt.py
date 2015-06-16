53e9308693341f3da1ca5f801d85353af009d8cf
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-06-16 09:36:57 -0600

    state if pdt install is succesful
    Former-commit-id: f41740c058896bb0e9e7cf2ebe48e83431e2729d
    Former-commit-id: 56e499f2ac4b0f995d8c14598cc940c2c6ea24d2
    Former-commit-id: d481d7377fae72f1e56d4a89cdf88b207bf4708c

--- packages/tau/cf/pdt.py
+++ packages/tau/cf/pdt.py
@@ -12,26 +12,26 @@
 #Copyright (c) 2015, ParaTools, Inc.
 #All rights reserved.
 #
-#Redistribution and use in source and binary forms, with or without 
+#Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are met:
-# (1) Redistributions of source code must retain the above copyright notice, 
+# (1) Redistributions of source code must retain the above copyright notice,
 #     this list of conditions and the following disclaimer.
-# (2) Redistributions in binary form must reproduce the above copyright notice, 
-#     this list of conditions and the following disclaimer in the documentation 
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
 #     and/or other materials provided with the distribution.
-# (3) Neither the name of ParaTools, Inc. nor the names of its contributors may 
-#     be used to endorse or promote products derived from this software without 
+# (3) Neither the name of ParaTools, Inc. nor the names of its contributors may
+#     be used to endorse or promote products derived from this software without
 #     specific prior written permission.
 #
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
-#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #"""
 
@@ -96,9 +96,8 @@ COMMANDS = {None: ['cparse',
                       'pdbtree',
                       'pdtf90disp',
                       'pdtflint',
-                      'tau_instrumentor',
                       'taucpdisp',
-                      'xmlgen'] 
+                      'xmlgen']
 }
 
 
@@ -130,13 +129,13 @@ class Pdt(object):
   def verify(self):
     """
     Returns true if if there is a working PDT installation at `prefix` with a
-    directory named `arch` containing `bin` and `lib` directories or 
+    directory named `arch` containing `bin` and `lib` directories or
     raises a ConfigurationError describing why that installation is broken.
     """
-    LOGGER.debug("Checking PDT installation at '%s' targeting arch '%s'" % (self.pdt_prefix, self.arch))    
+    LOGGER.debug("Checking PDT installation at '%s' targeting arch '%s'" % (self.pdt_prefix, self.arch))
     if not os.path.exists(self.pdt_prefix):
       raise error.ConfigurationError("'%s' does not exist" % self.pdt_prefix)
-  
+
     # Check for all commands
     try:
       commands = COMMANDS[self.arch]
@@ -150,17 +149,18 @@ class Pdt(object):
         raise error.ConfigurationError("'%s' is missing" % path)
       if not os.access(path, os.X_OK):
         raise error.ConfigurationError("'%s' exists but is not executable" % path)
-    
+
     LOGGER.debug("PDT installation at '%s' is valid" % self.pdt_prefix)
+    LOGGER.info("PDT installation at '%s' is valid" % self.pdt_prefix)
     return True
 
   def install(self, force_reinstall=False):
     """
     TODO: Docs
     """
-    LOGGER.debug("Initializing PDT at '%s' from '%s' with arch=%s" % 
+    LOGGER.debug("Initializing PDT at '%s' from '%s' with arch=%s" %
                  (self.pdt_prefix, self.src, self.arch))
-    
+
     # Check if the installation is already initialized
     if not force_reinstall:
       try:
@@ -219,7 +219,7 @@ class Pdt(object):
       # Always clean up PDT source
       LOGGER.debug('Deleting %r' % srcdir)
       shutil.rmtree(srcdir, ignore_errors=True)
-         
+
     # Verify the new installation
     try:
       retval = self.verify()
@@ -243,4 +243,4 @@ class Pdt(object):
     """
     pass
 
-    
+
