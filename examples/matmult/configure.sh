3e6c4e9e18c9d1e2788a6c043ee26d7261d66b1d
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-07-08 16:18:58 -0600

    added new configure
    Former-commit-id: 1762b79b72943d81eec37ba5ada7682972b84389
    Former-commit-id: e4af32c3734aef247926aeb19a6f2725c1953a8f
    Former-commit-id: 0f8df20b1e7e97720ad02178ac054a533b504349

---
+++ examples/matmult/configure.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if ! which tau >/dev/null 2>&1 ; then
+  echo "ERROR: 'tau' not found in PATH"
+  exit 1
+fi
+
+# Show commands as executed
+set +x
+
+# Example targets
+target_name="ex-`echo $HOSTNAME | cut -d. -f1`"
+tau target create "$target_name"
+user_name="`users`"
+
+sv_sys=false
+if [[ $target_name == *"Para"* ]] && [[ $user_name == *"srinath"* ]]
+then
+  sv_sys=true;
+  echo "This is a srinathv Paratools system"
+fi
+
+has_mic=false
+if [[ $MIC_ENV_PREFIX == *"MIC"*]]
+then
+  has_mic=true;
+  echo "This machine has MICs."
+fi
+
+#if on srinath systems:
+if $sv_sys; then
+  echo "** making gcc5 a target with now bfd and libunwind"
+  tau target create gcc5 --compilers=GNU --with-bfd=False --with-libunwind=False
+fi
+
+# Example applications
+tau application create "ex-matmult-serial"
+tau application create "ex-matmult-openmp" --openmp
+tau application create "ex-matmult-openmp-mpi" --openmp --mpi
+
+# Example measurements
+tau measurement create "ex-profile"
+tau measurement create "ex-trace" --profile=F --trace=T
+tau measurement create "ex-sample" --profile=F --sample=T
+tau measurement create "p-keep" --keep-inst-files=T
+tau measurement create ex-io --io-wrapper=T
+
+# Set up example project
+tau project create "ex-mm" \
+  $target_name \
+  ex-matmult-serial ex-matmult-openmp ex-matmult-openmp-mpi \
+  ex-profile ex-trace ex-sample ex-io
+
+if $sv_sys; then
+  echo "** making gcc target part of project"
+  tau project edit ex-mm --add-targets gcc5
+  tau project edit ex-mm --add-measurement p-keep
+fi
+
+tau dashboard
