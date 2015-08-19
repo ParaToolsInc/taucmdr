dc45eb8004ef0d96952e2d25bdabc4f66c850b25
John C. Linford <jlinford@paratools.com>
2015-08-19 12:22:48 -0400

    Update examples configure.sh
    Former-commit-id: afdc79166f606aded13fedcc369e0092ed91ec11
    Former-commit-id: 54a165153b818922f407f2f1292b182027ed88ac
    Former-commit-id: ab730b9595fc9b63ab5fdb1a965451dda9cdd713

--- examples/mm/configure.sh
+++ examples/mm/configure.sh
@@ -14,6 +14,7 @@ tau target create localhost
 # Example applications
 tau application create "ex-mm-serial"
 tau application create "ex-mm-openmp" --openmp
+tau application create "ex-mm-mpi" --mpi
 tau application create "ex-mm-openmp-mpi" --openmp --mpi
 
 # Example measurements
@@ -22,7 +23,7 @@ tau measurement create "ex-trace" --profile=F --trace=T
 tau measurement create "ex-sample" --source-inst=never --compiler-inst=never --sample=T
 
 # Set up example project
-tau project create "ex-mm" localhost ex-mm-serial ex-mm-openmp ex-mm-openmp-mpi ex-profile ex-trace ex-sample
-tau project select ex-mm ex-mm-openmp ex-profile
+tau project create "ex-mm" localhost ex-mm-serial ex-mm-openmp ex-mm-mpi ex-mm-openmp-mpi ex-profile ex-trace ex-sample
+tau project select ex-mm ex-mm-mpi ex-profile
   
 tau dashboard
