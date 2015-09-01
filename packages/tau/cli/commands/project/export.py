ca0ce39c31dd36aca3861c7687eaa09e5961f2c1
John C. Linford <jlinford@paratools.com>
2015-09-01 10:01:00 -0400

    Adding project export command.
    Former-commit-id: 19c23d412c3cfe84ca98d07090eb2a579a652f43
    Former-commit-id: 6704395469d1b896181ca99d9c36329e3d0db73f
    Former-commit-id: dca2d6a21e2f10de14ec827e77b7e2b858944998

---
+++ packages/tau/cli/commands/project/export.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015, ParaTools, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# (1) Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+# (3) Neither the name of ParaTools, Inc. nor the names of its contributors may
+#     be used to endorse or promote products derived from this software without
+#     specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+"""``tau project export`` subcommand."""
+
+from pprint import pprint
+from tau import logger, cli 
+from tau import EXIT_SUCCESS
+from tau.cli import arguments
+from tau.model.project import Project
+
+
+LOGGER = logger.get_logger(__name__)
+
+COMMAND = cli.get_command(__name__)
+
+SHORT_DESCRIPTION = "Export project configurations."
+
+HELP = """
+'%(command)s' page to be written.
+""" % {'command': COMMAND}
+
+
+def parser():
+    """Construct a command line argument parser.
+    
+    Constructing the parser may cause a lot of imports as :py:mod:`tau.cli` is explored.
+    To avoid possible circular imports we defer parser creation until afer all
+    modules are imported, hence this function.  The parser instance is maintained as
+    an attribute of the function, making it something like a C++ function static variable.
+    """
+    if not hasattr(parser, 'inst'):
+        usage_head = "%s project_name [project_name ...]" % COMMAND
+        parser.inst = arguments.get_parser(prog=COMMAND,
+                                           usage=usage_head,
+                                           description=SHORT_DESCRIPTION)
+        parser.inst.add_argument('names', 
+                                 help="Names of project to export",
+                                 metavar='project_name',
+                                 nargs='+')
+    return parser.inst
+
+
+def main(argv):
+    """Subcommand program entry point.
+    
+    Args:
+        argv (list): Command line arguments.
+        
+    Returns:
+        int: Process return code: non-zero if a problem occurred, 0 otherwise
+    """
+    argparser = parser()
+    args = argparser.parse_args(args=argv)
+    LOGGER.debug('Arguments: %s', args)
+
+    projects = []
+    for name in args.names: 
+        found = Project.with_name(name)
+        if not found:
+            argparser.error("'%s' is not a project name." % name)
+        projects.append(found)
+    pprint(Project.export_records(eids=[proj.eid for proj in projects]))
+    
+    return EXIT_SUCCESS
