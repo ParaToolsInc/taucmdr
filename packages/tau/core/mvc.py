a115e5ccded1464b51f8e6bc71151b996ccbe45a
John C. Linford <jlinford@paratools.com>
2015-09-29 17:27:10 -0400

    Fix with_key_attribute decorator.
    Former-commit-id: 46f90fa3dc04638aa4f80c4d2bee78909e801709
    Former-commit-id: 47dbf31010746ca45ae44f7e3d4e7b0c90898238
    Former-commit-id: d0d8991023e26fbb74bb2687241374356d5380af

--- packages/tau/core/mvc.py
+++ packages/tau/core/mvc.py
@@ -1133,24 +1133,8 @@ class Controller(object):
 
 
 def with_key_attribute(key_attr):
-    """Create a mixin class for models with a key attribute.
-    
-    Args:
-        key_attr (str): Name of the key attribute.
-        
-    Returns:
-        class: A Controller mixin class as shown in the example below.
-        
-    Example:
-        with_key_attribute('name') ==>
-        class WithName(object):
-            key_attribute = 'name'
-            def with_name(self, name):
-                return self.one({'name': name)
-    """
-    def with_key(self, key):
-        return self.one({key_attr: key})
-    name = util.camelcase('With'+key_attr)
-    dct = {'with_'+key_attr.lower(): with_key, 'key_attribute': key_attr}
-    return type(name, (object,), dct)
-
+    """Set a model controller's key attribute."""
+    def wrap(cls):
+        cls.key_attribute = key_attr
+        return cls
+    return wrap
