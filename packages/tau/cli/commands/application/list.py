6adcf463051808ca8828f8177f6fa9960b96c68e
John C. Linford <jlinford@paratools.com>
2015-11-04 15:28:44 -0500

    Fix MPI status in application dashboard

--- packages/tau/cli/commands/application/list.py
+++ packages/tau/cli/commands/application/list.py
@@ -34,7 +34,7 @@ from tau.model.application import Application
 DASHBOARD_COLUMNS = [{'header': 'Name', 'value': 'name', 'align': 'r'},
                      {'header': 'OpenMP', 'yesno': 'openmp'},
                      {'header': 'Pthreads', 'yesno': 'pthreads'},
-                     {'header': 'MPI', 'yesno': 'pthreads'},
+                     {'header': 'MPI', 'yesno': 'mpi'},
                      {'header': 'In Projects', 'function': lambda x: ', '.join([p['name'] for p in x['projects']])}]
 
 COMMAND = ListCommand(Application, __name__, dashboard_columns=DASHBOARD_COLUMNS)
