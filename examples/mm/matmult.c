b324fa616ddcee5dcc99519090924812c74bc329
John C. Linford <jlinford@paratools.com>
2015-11-16 11:24:46 -0500

    Prep matmult for SC'15 demo

--- examples/mm/matmult.c
+++ examples/mm/matmult.c
@@ -1,13 +1,13 @@
 /******************************************************************************
-*   OpenMp Example - Matrix Multiply - C Version
-*   Demonstrates a matrix multiply using OpenMP.
-*
-*   Modified from here:
-*   https://computing.llnl.gov/tutorials/openMP/samples/C/omp_mm.c
-*
-*   For  PAPI_FP_INS, the exclusive count for the event:
-*   for (null) [OpenMP location: file:matmult.c ]
-*   should be  2E+06 / Number of Threads
+ *   OpenMp Example - Matrix Multiply - C Version
+ *   Demonstrates a matrix multiply using OpenMP.
+ *
+ *   Modified from here:
+ *   https://computing.llnl.gov/tutorials/openMP/samples/C/omp_mm.c
+ *
+ *   For  PAPI_FP_INS, the exclusive count for the event:
+ *   for (null) [OpenMP location: file:matmult.c ]
+ *   should be  2E+06 / Number of Threads
 ******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,7 +18,7 @@
 int provided;
 #include <mpi.h>
 /* NOTE: MPI is just used to spawn multiple copies of the kernel to different ranks.
-This is not a parallel implementation */
+   This is not a parallel implementation */
 #endif /* TAU_MPI */
 
 #ifdef PTHREADS
@@ -29,6 +29,7 @@ This is not a parallel implementation */
 pthread_mutex_t mutexsum;
 #endif /* PTHREADS */
 
+#define APP_USE_INLINE_MULTIPLY 1
 
 #ifndef MATRIX_SIZE
 #define MATRIX_SIZE 512
@@ -56,39 +57,32 @@ void freeMatrix(double** matrix, int rows, int cols) {
 }
 
 __inline double multiply(double a, double b) {
-	return a * b;
+  return a * b;
 }
 
-#if 0
+#ifdef TAU_OPENMP
 // cols_a and rows_b are the same value
 void compute_nested(double **a, double **b, double **c, int rows_a, int cols_a, int cols_b) {
   int i,j,k;
   double tmp = 0.0;
-//num_threads(2)
-#pragma omp parallel private(i) shared(a,b,c)
-  {
-    /*** Do matrix multiply sharing iterations on outer loop ***/
-    /*** Display who does which iterations for demonstration purposes ***/
-#pragma omp for nowait schedule(dynamic,1)
-    for (i=0; i<rows_a; i++) {
-//num_threads(2)
-#pragma omp parallel private(i,j,k) shared(a,b,c)
-      {
-#pragma omp for nowait schedule(dynamic,1)
-        for (k=0; k<cols_a; k++) {
-          for(j=0; j<cols_b; j++) {
+  /*** Do matrix multiply sharing iterations on outer loop ***/
+  /*** Display who does which iterations for demonstration purposes ***/
+#pragma omp parallel for private(i,j,k) shared(a,b,c)
+  for (i=0; i<rows_a; i++) {
+    {
+      for (k=0; k<cols_a; k++) {
+        for(j=0; j<cols_b; j++) {
 #ifdef APP_USE_INLINE_MULTIPLY
-              c[i][j] += multiply(a[i][k], b[k][j]);
+          c[i][j] += multiply(a[i][k], b[k][j]);
 #else
-              tmp = a[i][k];
-			  tmp = tmp * b[k][j];
-              c[i][j] += tmp;
+          tmp = a[i][k];
+          tmp = tmp * b[k][j];
+          c[i][j] += tmp;
 #endif
-            }
-          }
+        }
       }
     }
-  }   /*** End of parallel region ***/
+  }
 }
 #endif
 
@@ -137,13 +131,13 @@ void compute_interchange(double **a, double **b, double **c, int rows_a, int col
 
 double do_work(void) {
   double **a,           /* matrix A to be multiplied */
-  **b,           /* matrix B to be multiplied */
-  **c;           /* result matrix C */
+         **b,           /* matrix B to be multiplied */
+         **c;           /* result matrix C */
   a = allocateMatrix(NRA, NCA);
   b = allocateMatrix(NCA, NCB);
   c = allocateMatrix(NRA, NCB);
 
-/*** Spawn a parallel region explicitly scoping all variables ***/
+  /*** Spawn a parallel region explicitly scoping all variables ***/
 
   initialize(a, NRA, NCA);
   initialize(b, NCA, NCB);
@@ -151,11 +145,9 @@ double do_work(void) {
 
   compute(a, b, c, NRA, NCA, NCB);
 #if defined(TAU_OPENMP)
-#if 0
   //if (omp_get_nested()) {
-    compute_nested(a, b, c, NRA, NCA, NCB);
+  compute_nested(a, b, c, NRA, NCA, NCB);
   //}
-#endif
 #endif
 #ifdef TAU_MPI
   if (provided == MPI_THREAD_MULTIPLE) {
@@ -225,7 +217,7 @@ int main (int argc, char *argv[])
   pthread_mutexattr_init(&Attr);
   pthread_mutexattr_settype(&Attr, PTHREAD_MUTEX_ERRORCHECK);
   if (pthread_mutex_init(&mutexsum, &Attr)) {
-   printf("Error while using pthread_mutex_init\n");
+    printf("Error while using pthread_mutex_init\n");
   }
 #endif /* PTHREADS */
 
@@ -270,7 +262,7 @@ int main (int argc, char *argv[])
 
 #endif /* PTHREADS */
 
-/* On thread 0: */
+  /* On thread 0: */
   int i;
   //for (i = 0 ; i < 100 ; i++) {
   do_work();
