e827a7c066aab2a74c0402f1093e135424b30eb8
John C. Linford <jlinford@paratools.com>
2016-02-16 16:51:19 -0500

    unittest seed

---
+++ packages/tau/tests/test_util.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class A1(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(1, 0)
+
+class A2(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(1, 0)
+
+class A3(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(1, 0)
+
+class A4(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(1, 0)
+
+class A5(unittest.TestCase):
+    def runTest(self):
+        self.assertEqual(1, 0)
+
