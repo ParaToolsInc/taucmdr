09f43c35c88f01fa83ec926939c831626f4bf670
Samuel Khuvis <skhuvis@paratools.com>
2016-02-29 12:32:38 -0500

    Add progress bar for downloads with wget & urllib.

--- packages/tau/util.py
+++ packages/tau/util.py
@@ -127,15 +127,17 @@ def download(src, dest):
         wget = which('wget')
         curl_cmd = [curl, '-L', src, '-o', dest] if curl else None
         wget_cmd = [wget, src, '-O', dest] if wget else None
-        for cmd in [curl_cmd, wget_cmd]:
+#        for cmd in [curl_cmd, wget_cmd]:
+        for cmd in [wget_cmd, curl_cmd]:
             if cmd:
-                if create_subprocess(cmd, stdout=False) == 0:
+                if create_dl_subprocess(cmd, stdout=False) == 0:
                     return
                 LOGGER.warning("%s failed to download '%s'. Retrying with a different method...", cmd[0], src)                    
         # Fallback: this is usually **much** slower than curl or wget
         def _dl_progress(count, block_size, total_size):
-            sys.stdout.write("% 3.1f%% of %d bytes\r" % (
-                min(100, float(count * block_size) / total_size * 100), total_size))
+            progress_bar(count*block_size, total_size)
+#            sys.stdout.write("% 3.1f%% of %d bytes\r" % (
+#                min(100, float(count * block_size) / total_size * 100), total_size))
         try:
             urllib.urlretrieve(src, dest, reporthook=_dl_progress)
         except Exception as err:
@@ -262,6 +264,94 @@ def create_subprocess(cmd, cwd=None, env=None, stdout=True, log=True):
     retval = proc.returncode
     LOGGER.debug("%s returned %d", cmd, retval)
     return retval
+
+
+def create_dl_subprocess(cmd, cwd=None, env=None, stdout=True, log=True):
+    """Create a subprocess for downloading software.
+    
+    See :any:`subprocess.Popen`.
+    
+    Args:
+        cmd (list): Command and its command line arguments.
+        cwd (str): Change directory to `cwd` if given, otherwise use :any:`os.getcwd`.
+        env (dict): Environment variables to set before launching cmd.
+        stdout (bool): If True send subprocess stdout and stderr to this processes' stdout.
+        log (bool): If True send subprocess stdout and stderr to the debug log.
+        
+    Returns:
+        int: Subprocess return code.
+    """
+    if not cwd:
+        cwd = os.getcwd()
+    if not env:
+        # Don't accidentally unset all environment variables with an empty dict
+        subproc_env = None
+    else:
+        subproc_env = dict(os.environ)
+        for key, val in env.iteritems():
+            subproc_env[key] = val
+            LOGGER.debug("%s=%s", key, val)
+    if 'curl' in cmd[0]:
+        print 'using curl'
+# Using shell = True
+#        file_size = subprocess.Popen('curl -sI ' + cmd[2] + ' --location | grep -i "content-length"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0]
+#        file_size = int(file_size.split()[1])
+# Using shell = False
+        size_command = 'curl -sI ' + cmd[2] + ' --location'
+        proc_output = subprocess.Popen(size_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+        file_size = int(proc_output.partition('Content-Length')[2].split()[1])
+        print file_size
+    if 'wget' in cmd[0]:
+        print 'using wget'
+# Using shell = True
+#        file_size = subprocess.Popen("wget " + cmd[1] + " --spider --server-response -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0]
+#        file_size = int(file_size)
+# Using shell = False
+        size_command = "wget " + cmd[1] + " --spider --server-response"
+        print size_command
+        proc_output = subprocess.Popen(size_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[1]
+        file_size = int(proc_output.partition('Content-Length')[2].split()[1])
+        print file_size
+    LOGGER.debug("Creating subprocess: cmd=%s, cwd='%s'\n", cmd, cwd)
+    proc = subprocess.Popen(cmd, cwd=cwd, env=subproc_env,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            bufsize=1)
+    with proc.stdout:
+        # Use iter to avoid hidden read-ahead buffer bug in named pipes:
+        # http://bugs.python.org/issue3907
+        for line in iter(proc.stdout.readline, b''):
+            if log:
+                LOGGER.debug(line[:-1])
+            if stdout:
+                print line,
+            try:
+                current_size = subprocess.Popen(["wc", "-c", cmd[-1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+                current_size = current_size.split()[0]
+            except:
+                current_size = 0
+#            print '%s of %s bytes' %(current_size, file_size)
+            progress_bar(current_size, file_size)
+    proc.wait()
+    retval = proc.returncode
+    LOGGER.debug("%s returned %d", cmd, retval)
+    return retval
+
+
+def progress_bar(current_size, total_size):
+    """Display progress bar for download of software
+
+    Args:
+        current_size (int):  current size of downloaded file
+        total_size (int): total size of downloaded file
+
+    Returns: 
+    """
+
+    size = logger.get_terminal_size()
+    width = int(size[0]) - 10
+    percent = min(100, float(current_size) / total_size)
+    sys.stdout.write('[' + '>' * int(percent * width) + '-' * int((1 - percent) * width) + '] %3s%%\r' %(int(100*percent)))
 
 
 def human_size(num, suffix='B'):
