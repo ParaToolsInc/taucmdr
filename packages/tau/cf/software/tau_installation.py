8ee834542d41aae3737a956a8b09f5661fdd467e
John C. Linford <jlinford@paratools.com>
2017-02-21 11:47:27 -0800

    Add workaround for TAU's broken Fortran wrapper for Intel MPI 17.0.1 20161005

--- packages/tau/cf/software/tau_installation.py
+++ packages/tau/cf/software/tau_installation.py
@@ -516,6 +516,8 @@ class TauInstallation(Installation):
         useropts = ['-O2', '-g']
         if self.target_arch is INTEL_KNL:
             useropts.append('-DTAU_MAX_THREADS=512')
+        # FIXME: Workaround for TAU's broken Fortran wrapper for Intel MPI 17.0.1 20161005
+        useropts.append('-DTAU_MPI_F_STATUSES_IGNORE_ABSENT=1') 
         flags.append('-useropt=%s' % '#'.join(useropts))
         if pdt:
             flags.append('-pdt=%s' % pdt.install_prefix)
