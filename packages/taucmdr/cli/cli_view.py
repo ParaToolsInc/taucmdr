3584d4c5de712313413192d95301e21cc44c844e
John C. Linford <jlinford@paratools.com>
2017-03-09 15:53:54 -0500

    Blinking text is really irritating.

--- packages/taucmdr/cli/cli_view.py
+++ packages/taucmdr/cli/cli_view.py
@@ -154,8 +154,7 @@ class DeleteCommand(AbstractCliView):
     def _construct_parser(self):
         key_attr = self.model.key_attribute
         usage = "%s <%s_%s> [arguments]" % (self.command, self.model_name, key_attr)       
-        epilog = util.color_text("WARNING: THIS OPERATION IS NOT REVERSABLE!",
-                                 'yellow', attrs=['bold', 'blink'])
+        epilog = util.color_text("WARNING: THIS OPERATION IS NOT REVERSABLE!", 'yellow', attrs=['bold'])
         parser = arguments.get_parser(prog=self.command,
                                       usage=usage,
                                       description=self.summary,
