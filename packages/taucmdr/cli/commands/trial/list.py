e248d47d0668b4acc908423e5d5ca08a0780eb2d
Samuel Khuvis <skhuvis@paratools.com>
2017-05-18 16:08:42 -0400

    Import InternalError to trial list.

--- packages/taucmdr/cli/commands/trial/list.py
+++ packages/taucmdr/cli/commands/trial/list.py
@@ -29,6 +29,7 @@
 
 from texttable import Texttable
 from taucmdr import util, logger
+from taucmdr.error import InternalError
 from taucmdr.cli.cli_view import ListCommand
 from taucmdr.model.project import Project
 from taucmdr.model.trial import Trial
