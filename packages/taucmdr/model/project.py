f4f50e49d912f222675bd59810764ecfaab7605c
Samuel Khuvis <skhuvis@paratools.com>
2017-05-25 12:13:03 -0400

    Fix for github issue #124.
    Check if PAPI metrics have been added/removed when updating a project.

--- packages/taucmdr/model/project.py
+++ packages/taucmdr/model/project.py
@@ -165,9 +165,16 @@ class Project(Model):
                                     new_path = new_comp['path'] if new_comp else None
                                     old_path = old_comp['path'] if old_comp else None
                                     message = {attr: (old_path, new_path)}
+                                    self.controller(self.storage).push_to_topic('rebuild_required', message)
+                                elif props.get('argparse').get('metavar') == '<metric>':
+                                    old_papi = [metric for metric in old_value if 'PAPI' in metric]
+                                    new_papi = [metric for metric in new_value if 'PAPI' in metric]
+                                    if bool(old_papi) != bool(new_papi):
+                                        message = {attr: (old_value, new_value)}
+                                        self.controller(self.storage).push_to_topic('rebuild_required', message)
                                 else:
                                     message = {attr: (old_value, new_value)}
-                                self.controller(self.storage).push_to_topic('rebuild_required', message)
+                                    self.controller(self.storage).push_to_topic('rebuild_required', message)
 
     @classmethod
     def controller(cls, storage=PROJECT_STORAGE):
