cdc1306ed294a95ab9f681759221f518f773fd0e
Samuel Khuvis <skhuvis@paratools.com>
2017-07-11 10:30:00 -0400

    Use TAU to generate OTF2 traces instead of Score-P.

---
+++ packages/taucmdr/cf/software/libotf2_installation.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015, ParaTools, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# (1) Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+# (3) Neither the name of ParaTools, Inc. nor the names of its contributors may
+#     be used to endorse or promote products derived from this software without
+#     specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+"""libotf2 software installation management.
+
+The OTF2 library  provides an interface to write and read trace data.
+"""
+
+import os
+import sys
+import fileinput
+from taucmdr import logger
+from taucmdr.cf.software.installation import AutotoolsInstallation
+
+
+
+LOGGER = logger.get_logger(__name__)
+
+REPOS = {None: 'http://www.vi-hps.org/upload/packages/otf2/otf2-2.1-rc2.tar.gz'}
+
+LIBRARIES = {None: ['libotf2.la', 'libotf2.a']}
+
+HEADERS = {None: ['otf2/otf2.h']}
+
+
+class Libotf2Installation(AutotoolsInstallation):
+    """Encapsulates a libotf2 installation."""
+
+    def __init__(self, sources, target_arch, target_os, compilers):
+        super(Libotf2Installation, self).__init__('libotf2', 'libotf2', sources, 
+                                                   target_arch, target_os, compilers, REPOS, None, LIBRARIES, HEADERS)
