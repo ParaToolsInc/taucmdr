1df94c326afcd7c4e5bd746817816d8019dd1d09
John C. Linford <jlinford@paratools.com>
2017-08-09 10:53:46 -0500

    Don't attempt TAU installation if Darshan is active.

--- packages/taucmdr/cf/software/tau_installation.py
+++ packages/taucmdr/cf/software/tau_installation.py
@@ -706,6 +706,9 @@ class TauInstallation(Installation):
         Raises:
             SoftwarePackageError: TAU failed installation or did not pass verification after it was installed.
         """
+        if 'DARSHAN_PRELOAD' in os.environ or 'darshan' in os.environ.get('LOADEDMODULES', '').lower():
+            raise ConfigurationError("TAU cannot be used with darshan."
+                                     "Please unload the darshan module and try again") 
         if self.forced_makefile:
             forced_install_prefix = os.path.abspath(os.path.join(os.path.dirname(self.forced_makefile), '..', '..'))
             self._set_install_prefix(forced_install_prefix)
