e5bc6b6a6e03cdf080698b174d9118ecd44a9300
John C. Linford <jlinford@paratools.com>
2017-09-05 11:51:21 -0400

    PEP 440 version compliance.

--- .version.sh
+++ .version.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if ! [ -f VERSION ] ; then
   version=`git describe --tags 2>/dev/null || echo "v0.0.0"`
+  if [ $version != "v0.0.0" ] ; then
+    #PEP 440 compliance
+    version=`echo $version | cut -d- -f1,2 | sed -e 's/-/./'`
+  fi
   echo ${version:1} > VERSION
 fi
 cat VERSION
