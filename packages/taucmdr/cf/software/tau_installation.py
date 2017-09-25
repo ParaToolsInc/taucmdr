96e2cadab131deb490a4076344a75d98e94ac541
Samuel Khuvis <skhuvis@paratools.com>
2017-09-25 11:34:28 -0400

    Expand to absolute path when opening traces
    Fixes issue #186.

--- packages/taucmdr/cf/software/tau_installation.py
+++ packages/taucmdr/cf/software/tau_installation.py
@@ -1261,6 +1261,7 @@ class TauInstallation(Installation):
         for path in paths:
             if not os.path.exists(path):
                 raise ConfigurationError("Trace file '%s' does not exist" % path)
+            path = os.path.abspath(path)
             cwd = os.path.dirname(path)
             retval += util.create_subprocess([os.path.join(self.bin_path, 'jumpshot'), path], 
                                              cwd=cwd, env=env, stdout=False)
@@ -1277,6 +1278,7 @@ class TauInstallation(Installation):
         for path in paths:
             if not os.path.exists(path):
                 raise ConfigurationError("Trace file '%s' does not exist" % path)
+            path = os.path.abspath(path)
             cwd = os.path.dirname(path)
             evt_files = glob.glob(os.path.join(cwd, 'traces/*.evt'))
             def_files = glob.glob(os.path.join(cwd, 'traces/*.def'))
