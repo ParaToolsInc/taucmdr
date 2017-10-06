c038f46d5e5b57b0562abb129a565b4f06c23fd3
John C. Linford <jlinford@paratools.com>
2017-10-06 17:37:05 -0400

    Whoops, add missing self.

--- packages/taucmdr/cf/software/tau_installation.py
+++ packages/taucmdr/cf/software/tau_installation.py
@@ -728,7 +728,7 @@ class TauInstallation(Installation):
         Raises:
             SoftwarePackageError: TAU failed installation or did not pass verification after it was installed.
         """
-        check_env_compat()
+        self.check_env_compat()
         if self.forced_makefile:
             forced_install_prefix = os.path.abspath(os.path.join(os.path.dirname(self.forced_makefile), '..', '..'))
             self._set_install_prefix(forced_install_prefix)
