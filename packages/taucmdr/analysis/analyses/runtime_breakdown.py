d3b2f1782d5212c42ede2709ad42062e8c837a53
Nicholas Chaimov <nchaimov@paratools.com>
2017-10-18 16:20:24 -0700

    Working Runtime Breakdown visualization
    Uses Bokeh to display a PerfExplorer-style Runtime Breakdown chart.
    Bokeh lacks native support for a stacked line chart, so this has to be
    done by generating polygons to draw into the figure for the filled lines.

--- packages/taucmdr/analysis/analyses/runtime_breakdown.py
+++ packages/taucmdr/analysis/analyses/runtime_breakdown.py
@@ -27,7 +27,14 @@
 #
 """PerfExplorer-style runtime breakdown chart"""
 
+import operator
+from collections import defaultdict
+import inspect
+
 from bokeh.models import ColumnDataSource
+import pandas as pd
+import numpy as np
+import nbformat
 
 from taucmdr.analysis.analysis import AbstractAnalysis
 from taucmdr.data.tauprofile import TauProfile
@@ -35,28 +42,27 @@ from taucmdr.error import ConfigurationError
 from taucmdr.gui.color import ColorMapping
 from taucmdr.model.trial import Trial
 
-import nbformat
-
-import inspect
-
 
 def show_runtime_breakdown(trials, metric):
-    from taucmdr.gui.interaction import InteractivePlotHandler
-    from taucmdr import logger
     from bokeh.plotting import figure
     from bokeh.io import output_notebook
+    from taucmdr.gui.interaction import InteractivePlotHandler
+    from taucmdr import logger
 
     logger.set_log_level('WARN')
     output_notebook(hide_banner=True)
 
     def build_runtime_breakdown(trials, metric):
-        patch_lists = RuntimeBreakdownVisualizer.trials_to_patch_lists(trials)
-        # TODO construct figure from patch lists
-        fig = figure(plot_width=80, plot_height=40, output_backend="webgl", toolbar_location="right")
+        patch_lists = RuntimeBreakdownVisualizer.trials_to_patch_lists(trials, metric)
+        title = trials[0].populate('experiment')['name']
+        fig = figure(title=title, plot_width=80, plot_height=40, output_backend="webgl", toolbar_location="right")
+        fig.xaxis.axis_label = 'Number of Processors'
+        fig.yaxis.axis_label = 'Percentage of Total Runtime'
+        fig.patches("x", "y", fill_color="color", line_color="color", alpha=0.9, source=patch_lists)
         return fig
 
     breakdown = build_runtime_breakdown(trials, metric)
-    plot = InteractivePlotHandler(breakdown)
+    plot = InteractivePlotHandler(breakdown, tooltips=[("Timer", "@name")])
     plot.show()
 
 
@@ -65,8 +71,66 @@ class RuntimeBreakdownVisualizer(AbstractAnalysis):
         super(RuntimeBreakdownVisualizer, self).__init__(name=name, description=description)
 
     @staticmethod
-    def trial_to_column_source(trials, metric):
-        pass
+    def trials_to_patch_lists(trials, metric):
+        sum_times = defaultdict(lambda: pd.DataFrame([], columns=[metric], dtype=np.float64))
+        timer_names = set()
+        # We need to get:
+        #     - Names for all of the timers in all of the trials (as we need a line for each timer)
+        #     - The total times for each trial across all processors
+        for trial in trials:
+            trial_num = trial['number']
+            trial_data = trial.get_data()
+            indices = TauProfile.indices(trial_data)
+            num_processors = len(indices)
+            trial_total_time = 0
+            trial_times = pd.DataFrame([], columns=[metric], dtype=np.float64)
+            for n, c, t in indices:
+                df = trial_data[n][c][t].interval_data()[[metric]].loc[trial_num, n, c, t]
+                trial_times = trial_times.add(df, fill_value=0.0)
+            for name, value in trial_times.itertuples():
+                timer_names.add(name)
+            # If there are multiple trials with the same num_processors, we aggregate them
+            sum_times[num_processors] = sum_times[num_processors].add(trial_times, fill_value=0.0)
+        # Convert the times to percentages
+        for num_processors in sum_times.keys():
+            sum_times[num_processors] = (sum_times[num_processors] / (sum_times[num_processors].sum())) * 100.0
+        # Now we need to convert the percentages to 'patches' for Bokeh to render.
+        # We need, for each timer, a list of x- and y-coordinates for the shape to be drawn.
+        # From x = 1 to num_processors, we trace out the line with y, then drop down to the
+        # previous line and draw it from x = num_processors to 1.
+        mapping = ColorMapping()
+        raw_xs = []
+        raw_ys = []
+        colors = []
+        names = []
+        sorted_sum_times = sorted(sum_times.items())
+        for timer_name in timer_names:
+            timer_xs = []
+            timer_ys = []
+            for num_processors, df in sorted_sum_times:
+                timer_xs.append(num_processors)
+                timer_ys.append(float(df.loc[timer_name]))
+            raw_xs.append(timer_xs)
+            raw_ys.append(timer_ys)
+            colors.append(mapping[timer_name])
+            names.append(timer_name)
+        # Postprocess the y values to stack the lines
+        ys = []
+        last = [0.0] * len(sorted_sum_times)
+        for row in raw_ys:
+            # Draw the line from left-to-right above the last line
+            processed_row = map(operator.add, row, last)
+            # Then complete the polygon by drawing along the top of the last line
+            return_row = last[::-1]
+            last = processed_row
+            ys.append(processed_row + return_row)
+        # Postprocess the x values for the return
+        xs = []
+        for row in raw_xs:
+            xs.append(row + row[::-1])
+        # Package everything into a data source
+        data_source = ColumnDataSource(dict(x=xs, y=ys, color=colors, name=names))
+        return data_source
 
     @staticmethod
     def _check_input(inputs, **kwargs):
@@ -102,8 +166,8 @@ class RuntimeBreakdownVisualizer(AbstractAnalysis):
                     inspect.getsource(show_runtime_breakdown)]
         def_cell_source = "\n".join(commands)
         notebook_cells.append(nbformat.v4.new_code_cell(def_cell_source))
-        trials_list_str = "[%s]" % (",".join(['Trial.controller(PROJECT_STORAGE).search_hash("%s")[0]' %
-                                              trial.hash_digest() for trial in trials]))
+        trials_list_str = "Trial.controller(PROJECT_STORAGE).search_hash([%s])" % (",".join(
+            ['"%s"' % trial.hash_digest() for trial in trials]))
         show_plot_str = 'show_runtime_breakdown(%s, "%s")' % (trials_list_str, metric)
         notebook_cells.append(nbformat.v4.new_code_cell(show_plot_str))
         return notebook_cells
