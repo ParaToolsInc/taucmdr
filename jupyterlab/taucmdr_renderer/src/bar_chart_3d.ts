dbdb60e38b34221cdf29bf18ecc8840b842f9bf6
Nicholas Chaimov <nchaimov@paratools.com>
2017-12-11 16:49:52 -0800

    More work on 3D graphics in JupyterLab

--- jupyterlab/taucmdr_renderer/src/bar_chart_3d.ts
+++ jupyterlab/taucmdr_renderer/src/bar_chart_3d.ts
@@ -25,53 +25,228 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import {
-    ReadonlyJSONValue
-} from '@phosphor/coreutils';
+import {ReadonlyJSONObject} from '@phosphor/coreutils';
 
 import {
-    Scene,
-    PerspectiveCamera,
-    WebGLRenderer,
+    AmbientLight,
     AxisHelper,
-    MeshBasicMaterial,
-    Mesh,
     BoxGeometry,
-    Color
+    Mesh,
+    MeshLambertMaterial,
+    PerspectiveCamera,
+    PointLight,
+    Scene,
+    WebGLRenderer,
 } from 'three';
 
+const DEFAULT_HEIGHT: number = 400;
+const DEFAULT_WIDTH: number = 400;
+const DEFAULT_FOV: number = 45;
+const DEFAULT_NEAR_CLIP: number = 0.1;
+const DEFAULT_FAR_CLIP: number = 1000;
+const DEFAULT_BG_COLOR: number = 0x000000;
+const DEFAULT_BG_ALPHA: number = 1.0;
+const DEFAULT_ANTIALIAS: boolean = true;
 
+/* Input settings for the 3D bar chart */
+export interface BarChart3DData {
+    height?: number;
+    width?: number;
+    fieldOfView?: number;
+    nearClip?: number;
+    farClip?: number;
+    bgColor?: number;
+    bgAlpha?: number;
+    antialias?: boolean;
+    xLabels?: Array<string>;
+    yLabels?: Array<string>;
+    zMin?: number;
+    zMax?: number;
+    values?: Array<number>;
+}
+
+/* Renderer for the 3D bar chart */
 export class BarChart3D {
 
-    private readonly data : ReadonlyJSONValue;
+    protected data: BarChart3DData;
 
-    constructor(data : ReadonlyJSONValue) {
-        this.data = data;
+    protected scene : Scene;
+    protected camera : PerspectiveCamera;
+    protected renderer : WebGLRenderer;
+
+    /* This needs to be a lambda because we need to call it from outside the
+    class context but need to access `this`. We create this once at instance creation
+    to avoid allocating a new lambda each time through the event loop. */
+    protected renderLoop = () => {
+        //this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.renderLoop);
+    };
+
+    constructor(data?: ReadonlyJSONObject) {
+        this.data = data || {};
     }
 
-    renderTo(div : HTMLDivElement) : void {
-        console.log("Rendering: " + this.data)
-        let scene = new Scene();
-        let camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-        let renderer = new WebGLRenderer();
-        renderer.setClearColor(new Color(0xEEEEEE), 1.0);
-        renderer.setSize(400, 400);
+    /* Accessors */
+
+    public get height(): number {
+        return this.data.height || DEFAULT_HEIGHT;
+    }
+
+    public set height(newHeight: number) {
+        this.data.height = newHeight;
+        this.updateCamera();
+    }
+
+    public get width(): number {
+        return this.data.width || DEFAULT_WIDTH;
+    }
+
+    public set width(newWidth: number) {
+        this.data.width = newWidth;
+        this.updateCamera();
+    }
+
+    public get fieldOfView(): number {
+        return this.data.fieldOfView || DEFAULT_FOV;
+    }
+
+    public set fieldOfView(newFieldOfView: number) {
+        this.data.fieldOfView = newFieldOfView;
+        if (this.camera) {
+            this.camera.fov = this.fieldOfView;
+            this.updateCamera();
+        }
+    }
+
+    public get nearClip(): number {
+        return this.data.nearClip || DEFAULT_NEAR_CLIP;
+    }
+
+    public set nearClip(newFarClip: number) {
+        this.data.nearClip = newFarClip;
+        if (this.camera) {
+            this.camera.near = this.farClip;
+            this.updateCamera();
+        }
+    }
+
+    public get farClip(): number {
+        return this.data.nearClip || DEFAULT_FAR_CLIP;
+    }
+
+    public set farClip(newFarClip: number) {
+        this.data.farClip = newFarClip;
+        if (this.camera) {
+            this.camera.far = this.farClip;
+            this.updateCamera();
+        }
+    }
+
+    public get bgColor(): number {
+        return this.data.bgColor || DEFAULT_BG_COLOR;
+    }
+
+    public set bgColor(newColor: number) {
+        this.data.bgColor = newColor;
+        if (this.renderer) {
+            this.renderer.setClearColor(this.bgColor, this.bgAlpha);
+        }
+    }
+
+    public get bgAlpha(): number {
+        return this.data.bgAlpha || DEFAULT_BG_ALPHA;
+    }
+
+    public set bgAlpha(newAlpha: number) {
+        this.data.bgAlpha = newAlpha;
+        if (this.renderer) {
+            this.renderer.setClearColor(this.bgColor, this.bgAlpha);
+        }
+    }
+
+    public get antialias(): boolean {
+        return this.data.antialias || DEFAULT_ANTIALIAS;
+    }
+
+    public get xLabels(): Array<string> {
+        return this.data.xLabels || [];
+    }
+
+    public get yLabels(): Array<string> {
+        return this.data.yLabels || [];
+    }
+
+    public get zMin(): number {
+        return this.data.zMin || 0;
+    }
+
+    public get zMax(): number {
+        return this.data.zMax || 0;
+    }
+
+    public get values(): Array<number> {
+        return this.data.values || [];
+    }
+
+    /* Rendering Functions */
+
+    public renderTo(div: HTMLDivElement): void {
+        console.log("Rendering");
+        console.log(this.data);
+
+        this.scene = new Scene();
+        this.camera = new PerspectiveCamera(this.fieldOfView, this.width / this.height, this.nearClip, this.farClip);
+        this.renderer = new WebGLRenderer({antialias: this.antialias});
+
+        this.renderer.setClearColor(this.bgColor, this.bgAlpha);
+        this.renderer.setSize(this.width, this.height);
+
         let axes = new AxisHelper(20);
-        scene.add(axes);
+        this.scene.add(axes);
+
+        // The ambient lights illuminates everything equally
+        let ambientLight = new AmbientLight(0x404040); // soft white light
+        this.scene.add(ambientLight);
+
+        // The point light illuminates from above the chart
+        let pointLight = new PointLight(0xFFFFFF, 1, 0);
+        pointLight.position.set(50, 50, 50);
+        this.scene.add(pointLight);
+
+
         let cubeGeometry = new BoxGeometry(4, 4, 4);
-        let cubeMaterial = new MeshBasicMaterial({color: 0xff0000, wireframe: true});
+        let cubeMaterial = new MeshLambertMaterial({color: 0xff0000, wireframe: false});
         let cube = new Mesh(cubeGeometry, cubeMaterial);
-        cube.position.x = -4;
-        cube.position.y = 3;
-        cube.position.z = 0;
-        scene.add(cube);
-        camera.position.x = -30;
-        camera.position.y = 40;
-        camera.position.z = 30;
-        camera.lookAt(scene.position);
-        div.appendChild(renderer.domElement);
-        // render the scene
-        renderer.render(scene, camera);
+        cube.position.set(-4, 3, 0);
+
+        this.scene.add(cube);
+        this.camera.position.set(-30, 40, 30);
+        this.camera.lookAt(this.scene.position);
+        div.appendChild(this.renderer.domElement);
+
+        /*this.controls = new TrackballControlsModule(this.camera);
+        this.controls.rotateSpeed = 1.0;
+        this.controls.zoomSpeed = 1.2;
+        this.controls.panSpeed = 0.8;
+        this.controls.noZoom = false;
+        this.controls.noPan = false;
+        this.controls.staticMoving = true;
+        this.controls.dynamicDampingFactor = 0.3;
+        */
+
+        // start rendering the scene
+        this.renderLoop();
+    }
+
+    protected updateCamera(): void {
+        if (this.camera) {
+            this.camera.aspect = this.width / this.height;
+            this.camera.updateProjectionMatrix();
+        }
+        if (this.renderer) {
+            this.renderer.setSize(this.width, this.height);
+        }
     }
 
 }
