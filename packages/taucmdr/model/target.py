d9d13b68977d81bbdde04921da95e5a0a607c488
devinshende <devin.s.shende@gmail.com>
2019-07-09 14:04:09 -0400

    Merge upstream

--- packages/taucmdr/model/target.py
+++ packages/taucmdr/model/target.py
@@ -168,6 +168,7 @@ def attributes():
     from taucmdr.cf.compiler.shmem import SHMEM_CC, SHMEM_CXX, SHMEM_FC
     from taucmdr.cf.compiler.cuda import CUDA_CXX, CUDA_FC
     from taucmdr.cf.compiler.caf import CAF_FC
+    from taucmdr.cf.compiler.python import PY
     
     knc_intel_only = _require_compiler_family(INTEL, 
                                               "You must use Intel compilers to target the Xeon Phi (KNC)",
@@ -351,11 +352,19 @@ def attributes():
             'description': 'Coarray Fortran compiler command',
             'argparse': {'flags': ('--caf-fc',),
                          'group': 'CAF',
+	},
+        PY.keyword: {
+            'model': Compiler,
+            'required': True,
+            'description': 'Python Interpreter command',
+            'argparse': {'flags': ('--python-interpreter',),
+                         'group': 'python',
                          'metavar': '<command>'},
             'rebuild_required': True
         },
         'cuda_toolkit': {
             'type': 'string',
+
             'description': 'path to NVIDIA CUDA Toolkit (enables OpenCL support)',
             'default': cuda_toolkit_default(), 
             'argparse': {'flags': ('--cuda-toolkit',),
