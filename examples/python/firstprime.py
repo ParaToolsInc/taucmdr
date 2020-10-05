c6f1f96b81f60e0d8392d3a7777baf2b664e0081
Izaak Beekman <zbeekman@gmail.com>
2020-10-05 18:09:28 -0400

    Fix style/convention issues & use of py3.6+ f str

--- examples/python/firstprime.py
+++ examples/python/firstprime.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
-#Program written by Devin Shende on August 5th 2018
+# Program written by Devin Shende on August 5th 2018
 
-def firstPrimeAfter(x):
-	import math
-	current = x + 1
-	sqrtno = math.sqrt(current)
-	while True:
-		#search for primes starting at x until it finds one
-		#break once found a prime
-		for potentialfactor in range(2,current):
-			# start at 2 because 1 will always be a factor
-			# go all the way up to the sqrt of current looking for a factor
-			if current % potentialfactor == 0:
-				# Found factor. not prime
-				break # move on to next number
-			elif potentialfactor >= sqrtno:
-				print(f"The first prime number after {x} is {current}")
-				return current
-		current += 1
+def first_prime_after(x):
+    import math
+    current = x + 1
+    sqrtno = math.sqrt(current)
+    while True:
+        # search for primes starting at x until it finds one
+        # break once found a prime
+        for potential_factor in range(2, current):
+            # start at 2 because 1 will always be a factor
+            # go all the way up to the sqrt of current looking for a factor
+            if current % potential_factor == 0:
+                # Found factor. not prime
+                break  # move on to next number
+            elif potential_factor >= sqrtno:
+                print("The first prime number after {} is {}".format(x, current))
+                return current
+        current += 1
 
-firstPrimeAfter(10000000)
+
+first_prime_after(10000000)
