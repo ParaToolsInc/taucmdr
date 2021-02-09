31ffeb33b00a23fdf8c1a75bc781576b181bffca
Izaak Beekman <zbeekman@gmail.com>
2021-02-09 11:35:37 -0500

    Remove errant print statement and bad line endings

--- packages/taucmdr/cf/software/tau_installation.py
+++ packages/taucmdr/cf/software/tau_installation.py
@@ -422,8 +422,8 @@ class TauInstallation(Installation):
         self.uses_pdt = not minimal and (self.source_inst == 'automatic' or self.shmem_support)
         self.uses_binutils = not minimal and (self.target_os is not DARWIN) and 'binutils' in sources
         self.uses_libunwind = not minimal and (self.target_os is not DARWIN) and 'libunwind' in sources
-        self.uses_libdwarf = not minimal and (self.target_os is not DARWIN) and 'libdwarf' in sources 
-        self.uses_libelf = not minimal and (self.target_os is not DARWIN) and 'libelf' in sources 
+        self.uses_libdwarf = not minimal and (self.target_os is not DARWIN) and 'libdwarf' in sources
+        self.uses_libelf = not minimal and (self.target_os is not DARWIN) and 'libelf' in sources
         self.uses_papi = not minimal and bool(len([met for met in self.metrics if 'PAPI' in met]))
         self.uses_scorep = not minimal and (self.profile == 'cubex')
         self.uses_ompt = not minimal and (self.measure_openmp == 'ompt')
@@ -902,7 +902,6 @@ class TauInstallation(Installation):
         if self.uses_python:
             flags.append('-python')
         if self.mpit:
-            print('append mpit')
             flags.append('-mpit')
 
         # Use -useropt for hacks and workarounds.
