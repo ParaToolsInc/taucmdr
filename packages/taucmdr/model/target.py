aa3e01d0c11b3bfce639ae34358168ed3388c563
Samuel Khuvis <skhuvis@paratools.com>
2021-10-24 13:13:29 -0400

    Check library path for libze loader in try-catch

--- packages/taucmdr/model/target.py
+++ packages/taucmdr/model/target.py
@@ -120,7 +120,10 @@ def papi_source_default():
     return 'download'
 
 def level_zero_source_default():
-    ld_lib_paths = os.environ['LD_LIBRARY_PATH'].split(':')
+    try:
+        ld_lib_paths = os.environ['LD_LIBRARY_PATH'].split(':')
+    except KeyError:
+        return 'download'
     for path in ld_lib_paths:
         if os.path.isfile(path+'/libze_loader.so'):
             return path
