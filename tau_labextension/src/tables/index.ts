4016039618e31905898940a9f44e57180177a927
PlatinumCD <Cameronfdurbin@gmail.com>
2022-03-15 20:41:07 -0500

    remove extension from taucmdr

--- tau_labextension/src/tables/index.ts
+++
@@ -1,5 +0,0 @@
-export { TargetTable } from './target';
-export { ApplicationTable } from './application';
-export { MeasurementTable } from './measurement';
-export { ExperimentTable } from './experiment';
-export { TrialTable, IPlotlyDisplayItem } from './trial';
